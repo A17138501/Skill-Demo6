96f0e939fdf292cf29c8b0df8dbfa9702d26b9d0
A17138501 <97008935+A17138501@users.noreply.github.com>
2022-05-01 23:45:32 -0700

    finsh code and test

--- SkillDemo6.java
+++ SkillDemo6.java
@@ -1,2 +1,5 @@
 public class SkillDemo6{
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
---
+++ skillDemotest6.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemotest6 {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
